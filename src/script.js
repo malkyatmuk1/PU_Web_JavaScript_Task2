cc05ab83983156ab902baa0d24a2ad0b811f6f42
yoana-yaneva <82182783+yoana-yaneva@users.noreply.github.com>
2021-11-19 21:59:03 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -12,43 +12,33 @@ function tax() {
     profitButton.addEventListener('click', calculateProfit);
 
     function calculateTax() {
-
         let income = Number(incomeElement.value);
         let secondIncome = Number(incomeSecondElement.value);
         let expense = Number(expenseElement.value);
         let secondExpense = Number(expenseSecondElement.value);
-
         if (income == '' || secondIncome == '' || expense == '' || secondExpense == '') {
-            return alert('All fields are required.')
+            return alert('All fields are required.');
         }
-
         if (income < 0 || secondIncome < 0 || expense < 0 || secondExpense < 0) {
             return alert('Values should be above zero.');
         }
-
         let taxResult = (income + secondIncome - expense - secondExpense) * 0.2;
         valueTaxElement.textContent = taxResult + 'лв';
     }
-
-
     function calculateProfit() {
         let income = Number(incomeElement.value);
         let secondIncome = Number(incomeSecondElement.value);
         let expense = Number(expenseElement.value);
         let secondExpense = Number(expenseSecondElement.value);
-
         if (income == '' || secondIncome == '' || expense == '' || secondExpense == '') {
-            return alert('All fields are required.')
+            return alert('All fields are required.');
         }
-
         if (income < 0 || secondIncome < 0 || expense < 0 || secondExpense < 0) {
             return alert('Values should be above zero.');
         }
-
         let taxResult = (income + secondIncome - expense - secondExpense) * 0.2;
         let profitResult = (income + secondIncome - expense - secondExpense) - taxResult;
         valueProfitElement.textContent = profitResult + 'лв';
-
     }
 }
 
